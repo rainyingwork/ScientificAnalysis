1dbf8f78210b9a666c0a227c004b81827719c546
rainyingwork <rainyingwork@gmail.com>
2022-12-03 23:43:50 +0800

    V 2.3.0 RPM 1.0.1
    更新匯入方法為批次匯入

--- ExerciseProject/RPM/circuit/P03Standard.py
+++ ExerciseProject/RPM/circuit/P03Standard.py
@@ -8,5 +8,5 @@ class Standard () :
         insertDataDF = pandas.read_csv("ExerciseProject/RPM/file/data/data.csv",converters={'CustomerID':str})
         insertDataDF['InvoiceNo'] = insertDataDF['InvoiceNo'].astype(str).str.replace(".0","")
         insertDataDF.columns = ['common_001', 'string_001', 'time_001', 'double_001']
-        StandardFunction.insertOverwriteStandardData("ExerciseProject", "RPM", "S0_0_1", "20220101", insertDataDF)
+        StandardFunction.insertOverwriteStandardData("ExerciseProject", "RPM", "S0_0_1", "20220101", insertDataDF,useType="IO")
         return {"result": "OK"}, {}
