706d02c9cb6f7faaf03343b5b5c7468f9feb4ff6
rainyingwork <rainyingwork@gmail.com>
2022-12-03 19:42:36 +0800

    V 2.2.2.1
    Pycaret 未來可用功能更新

--- Example/P33PycaretUse/Example_P33PycaretUse_02_MakeRunModel_V0_0_1.py
+++ Example/P33PycaretUse/Example_P33PycaretUse_02_MakeRunModel_V0_0_1.py
@@ -14,6 +14,24 @@ if __name__ == "__main__":
         }
         opsInfo = copy.deepcopy(basicInfo)
         opsInfo["OPSVersion"] = ["V0_0_1"]
+        opsInfo["OPSOrderJson"] = {
+            "ExeFunctionArr": ["R0_0_1","P0_0_1","M0_0_1","R0_0_2","P0_0_2","M0_0_2"]
+            , "RepOPSRecordId": 1098
+            , "RepFunctionArr": ["R0_0_1","P0_0_1","M0_0_1","R0_0_2","P0_0_2"]
+            , "RunFunctionArr": ["M0_0_2"]
+            , "OrdFunctionArr": [
+                {"Parent": "R0_0_1", "Child": "P0_0_1"}
+                , {"Parent": "P0_0_1", "Child": "M0_0_1"}
+                , {"Parent": "M0_0_1", "Child": "R0_0_2"}
+                , {"Parent": "R0_0_2", "Child": "P0_0_2"}
+                , {"Parent": "P0_0_2", "Child": "M0_0_2"}
+            ]
+            , "FunctionMemo": {
+                "R0_0_1": "撈取相關資料"
+                , "P0_0_1": "處理相關資料"
+                , "M0_0_1": "參數過濾"
+            }
+        }
         opsInfo["ParameterJson"] = {
             "R0_0_1": {
                 "FunctionType": "GetXYData"
---
+++ Example/P33PycaretUse/Readme.md
@@ -0,0 +1,5 @@
+未來可以加的功能
+    
+convert_model(model, language='python') 將模型變成相關的代碼，但不夠完整，有些模型不支援
+
+models 可以看所有相關的模型
