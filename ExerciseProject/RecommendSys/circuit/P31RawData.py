46dfea218d7590e78190718c724383053e09af8b
rainyingwork <rainyingwork@gmail.com>
2022-12-04 02:07:53 +0800

    V 2.3.1.4 RecommendSys 1.0.3
    更新檔案位置

--- ExerciseProject/RecommendSys/circuit/P31RawData.py
+++ ExerciseProject/RecommendSys/circuit/P31RawData.py
@@ -1,32 +1,7 @@
+from ExerciseProject.RecommendSys.circuit.CorrelationAnalysis.P31RawData import RawData as RawData_CorrelationAnalysis
 
-class RawData() :
+class RawData(RawData_CorrelationAnalysis) :
+    pass
 
-    @classmethod
-    def R0_0_1(self, functionInfo):
-        import copy
-        from package.artificialintelligence.common.rawdata.RawDataFunction import RawDataFunction
-        rawDataFunction = RawDataFunction()
-        functionVersionInfo = copy.deepcopy(functionInfo["ParameterJson"]["R0_0_1"])
-        functionVersionInfo["Version"] = "R0_0_1"
-        functionVersionInfo["SQLStrs"] = """
-            select
-                AA.common_001 as CustomerID
-                , AA.common_002 as Country
-                , AA.string_001 as InvoiceNo
-                , AA.string_002 as StockCode
-                , AA.string_003 as Description
-                , AA.integer_001 as Quantity
-                , AA.double_001 as UnitPrice
-                , AA.time_001 as InvoiceDate
-            from observationdata.standarddata AA
-            where 1 = 1
-                and AA.product = 'ExerciseProject'
-                and AA.project = 'RecommendSys'
-                and AA.tablename = 'S0_0_1'
-                and AA.dt = '20220101' 
-                and AA.common_002 = 'France' ; 
-        """
-        functionVersionInfo["SQLReplaceArr"] = rawDataFunction.getCommonSQLReplaceArr(functionInfo, functionVersionInfo)
-        resultObject, globalObjectDict = rawDataFunction.executionFunctionByFunctionType(functionVersionInfo)
-        return resultObject, globalObjectDict
+
 
--- ExerciseProject/RecommendSys/circuit/P33ModelUse.py
+++ ExerciseProject/RecommendSys/circuit/P33ModelUse.py
@@ -1,19 +1,4 @@
+from ExerciseProject.RecommendSys.circuit.CorrelationAnalysis.P33ModelUse import ModelUse as ModelUse_CorrelationAnalysis
 
-class ModelUse() :
-    
-    @classmethod
-    def M0_0_1(self, functionInfo):
-        import copy
-        from package.common.osbasic.GainObjectCtrl import GainObjectCtrl
-        from mlxtend.frequent_patterns import apriori
-        from mlxtend.frequent_patterns import association_rules
-        functionVersionInfo = copy.deepcopy(functionInfo["ParameterJson"]["M0_0_1"])
-        functionVersionInfo["Version"] = "M0_0_1"
-        globalObjectFDict = GainObjectCtrl.getObjectsById(functionInfo["GlobalObject"])["P0_0_1"]
-        basketSMDF = globalObjectFDict["ResultDF"]
-        # 使用 apriori 做出相關的關聯組合表
-        # 相關 apriori 使用方式可以參考 https://ithelp.ithome.com.tw/articles/10218530
-        basketSMFilterDF = apriori(basketSMDF, min_support=0.06, use_colnames=True)
-        resultDF = association_rules(basketSMFilterDF, metric="lift", min_threshold=1)
-        resultDF.sort_values('lift', ascending=False)
-        return {},{"ResultDF":basketSMDF}
+class ModelUse(ModelUse_CorrelationAnalysis) :
+    pass
