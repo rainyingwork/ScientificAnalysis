a589c4960c7d1e8dcbb2458a2b9e253e69f71f05
VicYing <58925394+rainyingwork@users.noreply.github.com>
2022-12-14 05:14:33 +0800

    V 3.1.0 Pytorch 1.2.0
    更新歌曲年代預測範例

--- UnitTest/PyTorch/circuit/P33ModelUse.py
+++ UnitTest/PyTorch/circuit/P33ModelUse.py
@@ -44,6 +44,121 @@ class ModelUse() :
         return {}, {}
 
     @classmethod
+    def M0_5_0(self, functionInfo):
+        import pandas as pd
+        from sklearn.model_selection import train_test_split
+        import torch
+        import torch.nn as nn
+        # 設定隨機種子
+        torch.manual_seed(0)
+
+        # 讀取資料
+        data = pd.read_csv('UnitTest/PyTorch/file/data/YearPredictionMSD.csv', nrows=50000, header=None)
+        print(data.head())
+        print(data.shape) # (50000, 91)
+
+        # 數據所有資料欄位
+        cols = data.columns
+        # 數據中所有為數值型態的資料欄位
+        num_cols = data._get_numeric_data().columns
+        print(list(set(cols) - set(num_cols))) # [] -> 代表所有都是數值欄位
+        print(data.isnull().sum().sum()) # 0 -> 代表資料非常乾淨沒有空值
+
+        outliers = []
+        for i in range(data.shape[1]):
+            min_t = data[data.columns[i]].mean() - (3 * data[data.columns[i]].std())
+            max_t = data[data.columns[i]].mean() + (3 * data[data.columns[i]].std())
+            count = 0
+            for j in data[data.columns[i]]:
+                if j < min_t or j > max_t:
+                    count += 1
+            percentage = count / data.shape[0]
+            if percentage > 0.05:
+                outliers.append(i)
+
+        print(outliers) # []
+
+        x = data.iloc[:, 1:]
+        y = data.iloc[:, 0]
+        x = (x - x.mean()) / x.std()
+
+        print(x.head())
+
+        # 拆分數據成2個子集，x_new : x_test = 80:20
+        # 再拆分數據集x_new成2個子集, x_train : x_dev = 75:25
+        x_new, x_test, y_new, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+        x_train, x_dev, y_train, y_dev = train_test_split(x_new, y_new, test_size=0.25, random_state=0)
+        print(x_train.shape, x_dev.shape, x_test.shape) # (30000, 90) (10000, 90) (10000, 90)
+
+
+        x_train_torch = torch.tensor(x_train.values).float()
+        y_train_torch = torch.tensor(y_train.values).float().unsqueeze(1) # 使用unsqueeze增加一個維度
+        x_dev_torch = torch.tensor(x_dev.values).float()
+        y_dev_torch = torch.tensor(y_dev.values).float().unsqueeze(1) # 使用unsqueeze增加一個維度
+        x_test_torch = torch.tensor(x_test.values).float()
+        y_test_torch = torch.tensor(y_test.values).float().unsqueeze(1) # 使用unsqueeze增加一個維度
+
+        print(x_train_torch.shape, y_train_torch.shape) # torch.Size([30000, 90]) torch.Size([30000, 1])
+
+        # 建立神經網路
+        model = nn.Sequential(
+            nn.Linear(x_train.shape[1], 200), # 輸入層
+            nn.ReLU(), # 激勵函數
+            nn.Linear(200, 50), # 中間層
+            nn.ReLU(), # 激勵函數
+            nn.Linear(50, 1) # 輸出層
+        )
+
+        device = "cpu"
+        model = model.to(device)
+        x_train_torch = x_train_torch.to(device)
+        y_train_torch = y_train_torch.to(device)
+        x_dev_torch = x_dev_torch.to(device)
+        y_dev_torch = y_dev_torch.to(device)
+
+        # 損失函數：使用MSELoss 與 學習函數：使用Adam
+        myloss = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+        # ---------- 模型訓練 ----------
+        epochs = 5001
+        for epoch in range(epochs):
+            # 切換成訓練模式
+            model.train()
+            y_pred = model(x_train_torch)
+            train_loss = myloss(y_pred, y_train_torch)
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+            if (epoch % 400 == 0):
+                with torch.no_grad():
+                    # 切換成驗證模式
+                    model.eval()
+                    y_pred2 = model(x_dev_torch)
+                    valid_loss = myloss(y_pred2, y_dev_torch)
+
+                # 可以注意到損失會不斷的下降
+                print(f"epoch={epoch},  train_loss:{train_loss.item():.3f},valid_loss:{valid_loss.item():.3f}")
+
+                # 損失值小於81 會提前結束訓練
+                if train_loss.item() < 81:
+                    break
+
+        # ---------- 模型驗證 ----------
+        model = model.to("cpu")
+        pred = model(x_test_torch)
+        test_loss = myloss(pred, y_test_torch)
+        print(f"test_loss: {test_loss.item():.3f}") # test_loss: 370.447
+        for i in range(100, 110):
+            print(f"truth:{y_test_torch[i].item():.0f}, pred:{pred[i].item():.0f}")
+
+        return {}, {}
+
+
+
+
+    @classmethod
     def M1_0_1(self, functionInfo):
         import copy
         import torch
@@ -138,7 +253,7 @@ class ModelUse() :
         print(f"Using {device} device")
         print(model)
 
-        # 損失函數：使用交叉熵誤差CrossEntropy 與 學習優化器optimizer
+        # 損失函數：使用交叉熵誤差CrossEntropy 與 學習函數：使用SGD 梯度下降
         lossFN = nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
