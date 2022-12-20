f52cfdec24dbdc5f4e6e096745e52b4e242af5bf
VicYing <58925394+rainyingwork@users.noreply.github.com>
2022-12-20 22:21:11 +0800

    V 3.5.2.30
    逗點更新

--- Example/P30PreProcess/Example_P30PreProcess_01_MakeBuildModel.py
+++ Example/P30PreProcess/Example_P30PreProcess_01_MakeBuildModel.py
@@ -5,21 +5,21 @@ import OPSCommon as executeOPSCommon
 
 if __name__ == "__main__":
     basicInfo = {
-        "RunType": ["buildops"]
-        , "Product": ["Example"]
-        , "Project": ["P30PreProcess"]
+        "RunType": ["buildops"],
+        "Product": ["Example"],
+        "Project": ["P30PreProcess"],
     }
     opsInfo = copy.deepcopy(basicInfo)
     opsInfo["OPSVersion"] = ["V0_0_1"]
     opsInfo["OPSOrderJson"] = {
-        "ExeFunctionArr": ["R0_0_1", "P0_0_1"]
-        , "OrdFunctionArr": [
-            {"Parent": "R0_0_1", "Child": "P0_0_1"}
-        ]
-        , "FunctionMemo": {
-            "R0_0_1": "撈取相關資料"
-            , "P0_0_1": "處理相關資料"
-        }
+        "ExeFunctionArr": ["R0_0_1", "P0_0_1"],
+        "OrdFunctionArr": [
+            {"Parent": "R0_0_1", "Child": "P0_0_1"},
+        ],
+        "FunctionMemo": {
+            "R0_0_1": "撈取相關資料",
+            "P0_0_1": "處理相關資料",
+        },
     }
     opsInfo["ParameterJson"] = {}
     opsInfo["ResultJson"] = {}
--- Example/P30PreProcess/Example_P30PreProcess_02_MakeRunModel.py
+++ Example/P30PreProcess/Example_P30PreProcess_02_MakeRunModel.py
@@ -8,26 +8,26 @@ if __name__ == "__main__":
     ]
     for dateInfo in dateInfoArr :
         basicInfo = {
-            "RunType": ["runops"]
-            , "Product": ["Example"]
-            , "Project": ["P30PreProcess"]
+            "RunType": ["runops"],
+            "Product": ["Example"],
+            "Project": ["P30PreProcess"],
         }
         opsInfo = copy.deepcopy(basicInfo)
         opsInfo["OPSVersion"] = ["V0_0_1"]
         opsInfo["ParameterJson"] = {
             "R0_0_1": {
-                "FunctionType": "GetXYData"
-                , "DataTime" : dateInfo['DataTime']
-                , "MakeDataKeys": ["common_001"]
-                , "MakeDataInfo": [
-                    {"DataType": "Y", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [1]}
-                    , {"DataType": "X", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]}
-                ]
-            }
-            , "P0_0_1": {
-                "FunctionType": "PPTagText"
-                , "DataTime": dateInfo['DataTime']
-                , "DataVersion" : "R0_0_1"
+                "FunctionType": "GetXYData",
+                "DataTime" : dateInfo['DataTime'],
+                "MakeDataKeys": ["common_001"],
+                "MakeDataInfo": [
+                    {"DataType": "Y", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [1]},
+                    {"DataType": "X", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]},
+                ],
+            },
+            "P0_0_1": {
+                "FunctionType": "PPTagText",
+                "DataTime": dateInfo['DataTime'],
+                "DataVersion" : "R0_0_1",
             }
         }
         opsInfo["ResultJson"] = {}
