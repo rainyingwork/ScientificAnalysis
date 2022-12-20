a116780daf0850e45b37d22670334993cacf78b3
VicYing <58925394+rainyingwork@users.noreply.github.com>
2022-12-20 22:22:57 +0800

    V 3.5.2.31
    逗點更新

--- Example/P31ScikitLearn/Example_P31ScikitLearn_01_MakeBuildModel.py
+++ Example/P31ScikitLearn/Example_P31ScikitLearn_01_MakeBuildModel.py
@@ -5,23 +5,23 @@ import OPSCommon as executeOPSCommon
 
 if __name__ == "__main__":
     basicInfo = {
-        "RunType": ["buildops"]
-        , "Product": ["Example"]
-        , "Project": ["P31ScikitLearn"]
+        "RunType": ["buildops"],
+        "Product": ["Example"],
+        "Project": ["P31ScikitLearn"],
     }
     opsInfo = copy.deepcopy(basicInfo)
     opsInfo["OPSVersion"] = ["V0_0_1"]
     opsInfo["OPSOrderJson"] = {
-        "ExeFunctionArr": ["R0_0_1","P0_0_1","M0_0_1"]
-        , "OrdFunctionArr": [
-            {"Parent": "R0_0_1", "Child": "P0_0_1"}
-            , {"Parent": "P0_0_1", "Child": "M0_0_1"}
-        ]
-        , "FunctionMemo": {
-            "R0_0_1": "撈取相關資料"
-            , "P0_0_1": "處理相關資料"
-            , "M0_0_1": "參數過濾"
-        }
+        "ExeFunctionArr": ["R0_0_1","P0_0_1","M0_0_1"],
+        "OrdFunctionArr": [
+            {"Parent": "R0_0_1", "Child": "P0_0_1"},
+            {"Parent": "P0_0_1", "Child": "M0_0_1"},
+        ],
+        "FunctionMemo": {
+            "R0_0_1": "撈取相關資料",
+            "P0_0_1": "處理相關資料",
+            "M0_0_1": "參數過濾",
+        },
     }
     opsInfo["ParameterJson"] = {}
     opsInfo["ResultJson"] = {}
--- Example/P31ScikitLearn/Example_P31ScikitLearn_02_MakeRunModel.py
+++ Example/P31ScikitLearn/Example_P31ScikitLearn_02_MakeRunModel.py
@@ -8,38 +8,38 @@ if __name__ == "__main__":
     ]
     for dateInfo in dateInfoArr :
         basicInfo = {
-            "RunType": ["runops"]
-            , "Product": ["Example"]
-            , "Project": ["P31ScikitLearn"]
+            "RunType": ["runops"],
+            "Product": ["Example"],
+            "Project": ["P31ScikitLearn"],
         }
         opsInfo = copy.deepcopy(basicInfo)
         opsInfo["OPSVersion"] = ["V0_0_1"]
         opsInfo["ParameterJson"] = {
             "R0_0_1": {
-                "FunctionType": "GetXYData"
-                , "DataTime" : dateInfo['DataTime']
-                , "MakeMaxColumnCount": 10
-                , "MakeDataKeys": ["common_001"]
-                , "MakeDataInfo": [
-                    {"DataType": "Y", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [1]}
-                    , {"DataType": "X", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]}
-                ]
-            }
-            , "P0_0_1": {
-                "FunctionType": "PPTagText"
-                , "DataTime": dateInfo['DataTime']
-                , "DataVersion" : "R0_0_1"
-            }
-            , "M0_0_1": {
-                "FunctionType":"TagFilter"
-                , "DataTime": dateInfo['DataTime']
-                , "DataVersion": "P0_0_1"
-                , "ModelFunction":"Lasso"
-                , "ModelParameter":{
-                    "TopK": 10
-                    , "Filter":0.000000001
-                }
-            }
+                "FunctionType": "GetXYData",
+                "DataTime" : dateInfo['DataTime'],
+                "MakeMaxColumnCount": 10,
+                "MakeDataKeys": ["common_001"],
+                "MakeDataInfo": [
+                    {"DataType": "Y", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [1]},
+                    {"DataType": "X", "Product": "Example", "Project": "P29RawData", "Version": "R0_0_1", "DTDiff": 0, "ColumnNumbers": [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]},
+                ],
+            },
+            "P0_0_1": {
+                "FunctionType": "PPTagText",
+                "DataTime": dateInfo['DataTime'],
+                "DataVersion" : "R0_0_1",
+            },
+            "M0_0_1": {
+                "FunctionType":"TagFilter",
+                "DataTime": dateInfo['DataTime'],
+                "DataVersion": "P0_0_1",
+                "ModelFunction":"Lasso",
+                "ModelParameter":{
+                    "TopK": 10,
+                    "Filter":0.000000001,
+                },
+            },
         }
         opsInfo["ResultJson"] = {}
         executeOPSCommon.main(opsInfo)
