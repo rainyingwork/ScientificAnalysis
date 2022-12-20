7bcd5e22eba4a02e8bcd06469db8aab19acb4efa
VicYing <58925394+rainyingwork@users.noreply.github.com>
2022-12-20 20:41:33 +0800

    V 3.5.2.1
    修改範例的逗點名稱

--- Example/P01Basic/Example_P01Basic_02_MakeRunModel.py
+++ Example/P01Basic/Example_P01Basic_02_MakeRunModel.py
@@ -4,33 +4,33 @@ import OPSCommon as executeOPSCommon
 
 if __name__ == "__main__":
     basicInfo = {
-        "RunType": ["runops"]
-        , "Product": ["Example"]
-        , "Project": ["P01Basic"]
+        "RunType": ["runops"] ,
+        "Product": ["Example"] ,
+        "Project": ["P01Basic"] ,
     }
     opsInfo = copy.deepcopy(basicInfo)
     opsInfo["OPSVersion"] = ["V0_0_1"]
     # opsInfo["OPSRecordId"] = []
     opsInfo["OPSOrderJson"] = {
-        "ExeFunctionArr": ["C0_0_1", "O0_0_1", "S0_0_1", "R0_0_1", "P0_0_1", "M0_0_1"]
-        # , "RepOPSRecordId": 0
-        # , "RepFunctionArr": [""]
-        # , "RunFunctionArr": [""]
-        , "OrdFunctionArr": [
-            {"Parent": "C0_0_1", "Child": "O0_0_1"}
-            , {"Parent": "O0_0_1", "Child": "S0_0_1"}
-            , {"Parent": "S0_0_1", "Child": "R0_0_1"}
-            , {"Parent": "R0_0_1", "Child": "P0_0_1"}
-            , {"Parent": "P0_0_1", "Child": "M0_0_1"}
-        ]
-        , "FunctionMemo": {
-            "C0_0_1": ""
-            , "O0_0_1": ""
-            , "S0_0_1": ""
-            , "R0_0_1": ""
-            , "P0_0_1": ""
-            , "M0_0_1": ""
-        }
+        "ExeFunctionArr": ["C0_0_1", "O0_0_1", "S0_0_1", "R0_0_1", "P0_0_1", "M0_0_1"] ,
+        # "RepOPSRecordId": 0 ,
+        # "RepFunctionArr": [""] ,
+        # "RunFunctionArr": [""] ,
+        "OrdFunctionArr": [
+            {"Parent": "C0_0_1", "Child": "O0_0_1"} ,
+            {"Parent": "O0_0_1", "Child": "S0_0_1"} ,
+            {"Parent": "S0_0_1", "Child": "R0_0_1"} ,
+            {"Parent": "R0_0_1", "Child": "P0_0_1"} ,
+            {"Parent": "P0_0_1", "Child": "M0_0_1"} ,
+        ],
+        "FunctionMemo": {
+            "C0_0_1": "",
+            "O0_0_1": "",
+            "S0_0_1": "",
+            "R0_0_1": "",
+            "P0_0_1": "",
+            "M0_0_1": "",
+        } ,
     }
     opsInfo["ParameterJson"] = {}
     opsInfo["ResultJson"] = {}
