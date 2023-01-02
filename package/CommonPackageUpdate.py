ae855b3f1c1ce9f4345e661f07a476d5d1811880
rainyingwork <rainyingwork@gmail.com>
2023-01-03 02:26:23 +0800

    V 3.1.2
    上傳完成PYPY安裝
    MAC安裝
    Windows安裝
    套件包更新

--- package/CommonPackageUpdate.py
+++ package/CommonPackageUpdate.py
@@ -1,36 +1,38 @@
 import os , shutil
 import sys
 
-pythonEXEPath = "bin" if os.name == "posix" else "Scripts"
-__pythonexePath = "{}/venv/{}/python".format(sys.path[1],pythonEXEPath)
+isInstall = False
+pythonEXEPath = "python" if os.path.isfile("venv/python.exe") else "bin/python" if os.name == "posix" else "Scripts/python"
+__pythonexePath = "{}/venv/{}".format(sys.path[1],pythonEXEPath)
+__pipFunction = "pip install" if isInstall else "pip download -d venv\pip"
 
 # Hadoop套件 -------------------------------------------------
-os.system("{} -m pip install impyla==0.18.0".format(__pythonexePath)) # 用於讀取Hive資料
-os.system("{} -m pip install hdfs==2.7.0".format(__pythonexePath)) # 用於讀取HDFS資料
-os.system("{} -m pip install pyspark==3.3.1".format(__pythonexePath)) # pyspark套件
-os.system("{} -m pip install py4j==0.10.9.7".format(__pythonexePath)) # pyspark相依套件
+os.system("{} -m {} impyla==0.18.0".format(__pythonexePath,__pipFunction))          # 用於讀取Hive資料
+os.system("{} -m {} hdfs==2.7.0".format(__pythonexePath,__pipFunction))             # 用於讀取HDFS資料
+os.system("{} -m {} pyspark==3.3.1".format(__pythonexePath,__pipFunction))          # pyspark套件
+os.system("{} -m {} py4j==0.10.9.5".format(__pythonexePath,__pipFunction))          # pyspark相依套件
 # 機器學習套件 -------------------------------------------------
-os.system("{} -m pip install scikit-learn==1.1.3".format(__pythonexePath)) # 機器學習套件
-os.system("{} -m pip install scikit-surprise==1.1.3".format(__pythonexePath)) # 機器學習套件
-os.system("{} -m pip install mlxtend==0.21.0".format(__pythonexePath)) # 機器學習套件
-os.system("{} -m pip install networkx==2.8.8".format(__pythonexePath)) # 網路分析
-os.system("{} -m pip install xgboost==1.6.2".format(__pythonexePath)) # 梯度提升樹
-os.system("{} -m pip install catboost==1.0.6".format(__pythonexePath)) # 梯度提升樹
-os.system("{} -m pip install lightgbm==3.3.1".format(__pythonexePath)) # 梯度提升樹
-os.system("{} -m pip install m2cgen==0.10.0".format(__pythonexePath)) # 模型轉換套件
-os.system("{} -m pip install evidently==0.2.0".format(__pythonexePath)) # 模型評估套件
+os.system("{} -m {} scikit-learn==1.1.3".format(__pythonexePath,__pipFunction))     # 機器學習套件
+os.system("{} -m {} scikit-surprise==1.1.3".format(__pythonexePath,__pipFunction))  # 機器學習套件
+os.system("{} -m {} mlxtend==0.21.0".format(__pythonexePath,__pipFunction))         # 機器學習套件
+os.system("{} -m {} networkx==2.8.8".format(__pythonexePath,__pipFunction))         # 網路分析
+os.system("{} -m {} xgboost==1.6.2".format(__pythonexePath,__pipFunction))          # 梯度提升樹
+os.system("{} -m {} catboost==1.0.6".format(__pythonexePath,__pipFunction))         # 梯度提升樹
+os.system("{} -m {} lightgbm==3.3.1".format(__pythonexePath,__pipFunction))         # 梯度提升樹
+os.system("{} -m {} m2cgen==0.10.0".format(__pythonexePath,__pipFunction))          # 模型轉換套件
+os.system("{} -m {} evidently==0.2.0".format(__pythonexePath,__pipFunction))        # 模型評估套件
 # 自然語言套件 -------------------------------------------------
-os.system("{} -m pip install nltk==3.8".format(__pythonexePath)) # 自然語言處理套件
-os.system("{} -m pip install rake_nltk==1.0.6".format(__pythonexePath)) # 自然語言處理套件
-os.system("{} -m pip install gensim==4.2.0".format(__pythonexePath)) # 自然語言處理套件
+os.system("{} -m {} nltk==3.8".format(__pythonexePath,__pipFunction))               # 自然語言處理套件
+os.system("{} -m {} rake_nltk==1.0.6".format(__pythonexePath,__pipFunction))        # 自然語言處理套件
+os.system("{} -m {} gensim==4.2.0".format(__pythonexePath,__pipFunction))           # 自然語言處理套件
 # Pytorch套件 -------------------------------------------------
-os.system("{} -m pip install torch==1.12.0".format(__pythonexePath))  # 深度學習套件
-os.system("{} -m pip install torchvision==0.13.0".format(__pythonexePath))  # 深度學習套件
-os.system("{} -m pip install torchaudio==0.13.0".format(__pythonexePath))  # 深度學習套件
+os.system("{} -m {} torch==1.13.0".format(__pythonexePath,__pipFunction))           # 深度學習套件
+os.system("{} -m {} torchvision==0.13.0".format(__pythonexePath,__pipFunction))     # 深度學習套件
+os.system("{} -m {} torchaudio==0.13.0".format(__pythonexePath,__pipFunction))      # 深度學習套件
 # Tensorflow 套件 -------------------------------------------------
-os.system("{} -m pip install tensorflow==2.11.0".format(__pythonexePath))
+os.system("{} -m {} tensorflow==2.11.0".format(__pythonexePath,__pipFunction))
 # 自動機器學習套件 -------------------------------------------------
-os.system("{} -m pip install pycaret==3.0.0rc2".format(__pythonexePath))# 自動機器學習套件
-os.system("{} -m pip install autokeras==1.0.20".format(__pythonexePath))  # 自動機器學習套件
+os.system("{} -m {} pycaret==3.0.0rc2".format(__pythonexePath,__pipFunction))       # 自動機器學習套件
+os.system("{} -m {} autokeras==1.0.20".format(__pythonexePath,__pipFunction))       # 自動機器學習套件
 # 其他套件 -------------------------------------------------
-os.system("{} -m pip install gym==0.26.2".format(__pythonexePath)) # 遊戲場套件
+os.system("{} -m {} gym==0.26.2".format(__pythonexePath,__pipFunction))             # 遊戲場套件
--- package/common/PackageInstall.py
+++ package/common/PackageInstall.py
@@ -3,40 +3,39 @@ import sys
 
 # 以 Python 3.9.13 為最該系統的版本
 
-pythonEXEPath = "bin" if os.name == "posix" else "Scripts"
-__pythonexePath = "{}/venv/{}/python".format(sys.path[1],pythonEXEPath)
+isInstall = False
+pythonEXEPath = "python" if os.path.isfile("venv/python.exe") else "bin/python" if os.name == "posix" else "Scripts/python"
+__pythonexePath = "{}/venv/{}".format(sys.path[1],pythonEXEPath)
+__pipFunction = "pip install" if isInstall else "pip download -d venv\pip"
 
-# # Python套件 -------------------------------------------
-# os.system("{} -m pip install pip==22.3.1".format(__pythonexePath))
-# os.system("{} -m pip install setuptools==65.6.3".format(__pythonexePath))
-# # 基本套件 SSH套件 -------------------------------------------
-# os.system("{} -m pip install gitpython==3.1.27".format(__pythonexePath))
-# os.system("{} -m pip install python-dotenv==0.21.0".format(__pythonexePath))
-# os.system("{} -m pip install paramiko==2.11.0".format(__pythonexePath))
-# # 資料處理套件 -----------------------------------------------
-# os.system("{} -m pip install pandas==1.4.4".format(__pythonexePath))
-# os.system("{} -m pip install numpy==1.22.4".format(__pythonexePath))
-# os.system("{} -m pip install scipy==1.8.1".format(__pythonexePath))
-# # 資料庫套件 -------------------------------------------------
-# os.system("{} -m pip install pyodbc==4.0.34".format(__pythonexePath))
-# os.system("{} -m pip install SQLAlchemy==1.4.41".format(__pythonexePath))
-# os.system("{} -m pip install {}==2.9.3".format(__pythonexePath ,"psycopg2-binary" if os.name == "posix" else "psycopg2"))
-# # GoogleAPI套件 -------------------------------------------------
-# os.system("{} -m pip install google-auth==2.15.0".format(__pythonexePath))
-# os.system("{} -m pip install oauth2client==4.1.3".format(__pythonexePath))
-# os.system("{} -m pip install google-auth-httplib2==0.1.0".format(__pythonexePath))
-# os.system("{} -m pip install google-auth-oauthlib==0.4.0".format(__pythonexePath))
-# os.system("{} -m pip install google-api-python-client==2.31.0".format(__pythonexePath))
-# # 其他套件 -------------------------------------------------
-# os.system("{} -m pip install matplotlib==3.6.0".format(__pythonexePath))  # 繪圖套件
-# os.system("{} -m pip install seaborn==0.12.1".format(__pythonexePath))  # 繪圖套件
-# os.system("{} -m pip install Pillow==9.3.0".format(__pythonexePath)) # 圖片處理套件
-# os.system("{} -m pip install Flask==2.2.2".format(__pythonexePath)) # 網頁套件
-# os.system("{} -m pip install streamlit==1.16.0".format(__pythonexePath)) # 網頁套件
-# os.system("{} -m pip install tqdm==4.64.1".format(__pythonexePath)) # 進度條套件
-# os.system("{} -m pip install openpyxl==3.0.10".format(__pythonexePath)) # Excel套件
-
-if os.name == "posix" :
-    os.system("{} -m pip install certifi==2022.12.7".format(__pythonexePath)) # 修正SSL錯誤
+# Python套件 -------------------------------------------
+os.system("{} -m {} pip==22.3.1".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} setuptools==65.6.3".format(__pythonexePath,__pipFunction))
+# 基本套件 SSH套件 -------------------------------------------
+os.system("{} -m {} gitpython==3.1.27".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} python-dotenv==0.21.0".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} paramiko==2.11.0".format(__pythonexePath,__pipFunction))
+# 資料處理套件 -----------------------------------------------
+os.system("{} -m {} pandas==1.4.4".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} numpy==1.22.4".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} scipy==1.8.1".format(__pythonexePath,__pipFunction))
+# 資料庫套件 -------------------------------------------------
+os.system("{} -m {} pyodbc==4.0.34".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} SQLAlchemy==1.4.41".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} {}==2.9.3".format(__pythonexePath,__pipFunction ,"psycopg2-binary" if os.name == "posix" else "psycopg2"))
+# GoogleAPI套件 -------------------------------------------------
+os.system("{} -m {} google-auth==2.15.0".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} oauth2client==4.1.3".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} google-auth-httplib2==0.1.0".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} google-auth-oauthlib==0.4.0".format(__pythonexePath,__pipFunction))
+os.system("{} -m {} google-api-python-client==2.31.0".format(__pythonexePath,__pipFunction))
+# 其他套件 -------------------------------------------------
+os.system("{} -m {} matplotlib==3.6.0".format(__pythonexePath,__pipFunction))           # 繪圖套件
+os.system("{} -m {} seaborn==0.12.1".format(__pythonexePath,__pipFunction))             # 繪圖套件
+os.system("{} -m {} Pillow==9.3.0".format(__pythonexePath,__pipFunction))               # 圖片處理套件
+os.system("{} -m {} Flask==2.2.2".format(__pythonexePath,__pipFunction))                # 網頁套件
+os.system("{} -m {} streamlit==1.16.0".format(__pythonexePath,__pipFunction))           # 網頁套件
+os.system("{} -m {} tqdm==4.64.1".format(__pythonexePath,__pipFunction))                # 進度條套件
+os.system("{} -m {} openpyxl==3.0.10".format(__pythonexePath,__pipFunction))            # Excel套件
 
 
