5210b2ecadca1d862d7411e96d5a619e76774c13
rainyingwork <rainyingwork@gmail.com>
2023-02-10 23:28:50 +0800

    V 3.5.4
    3.5版 P23Standard UnitTest完成

--- Example/P23Standard/Example_P23Standard_02_RunOPS.py
+++ Example/P23Standard/Example_P23Standard_02_RunOPS.py
@@ -1,15 +1,24 @@
 import os , copy
 import Config
-import OPSCommon as executeOPSCommon
+import OPSCommonLocal as executeOPSCommon
 
 if __name__ == "__main__":
     basicInfo = {
-        "RunType": ["runops"],
+        "RunType": ["RunOPS"],
         "Product": ["Example"],
         "Project": ["P23Standard"],
     }
     opsInfo = copy.deepcopy(basicInfo)
     opsInfo["OPSVersion"] = ["V0_0_1"]
+    opsInfo["OPSRecordId"] = [9999]
+    opsInfo["OPSOrderJson"] = {
+        "ExeFunctionArr": ["S0_0_1"],
+        "OrdFunctionArr": [
+        ],
+        "FunctionMemo": {
+            "S0_0_1": "Juice資料塞入正規資料庫",
+        },
+    }
     opsInfo["ParameterJson"] = {
         "S0_0_1": {}
     }
